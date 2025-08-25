5a7d23a03333616a88962b315facf38dc715918d
Edouard Nuñez <lc22140269@queretaro.tecnm.mx>
2025-08-25 13:47:52 -0600

    Modificar el método main para crear y mostrar la interfaz de la calculadora

--- Calculadora1/src/main/java/com/mycompany/calculadora1/Calculadora1.java
+++ Calculadora1/src/main/java/com/mycompany/calculadora1/Calculadora1.java
@@ -11,6 +11,7 @@ package com.mycompany.calculadora1;
 public class Calculadora1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
     }
 }
